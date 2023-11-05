235d54eec7a874a2ad51bdfd4250cdf85f663447
paradise-j <warastudio61@gmail.com>
2023-11-05 21:42:17 +0700

    Update index.php

--- role/admin/index.php
+++ role/admin/index.php
@@ -12,6 +12,7 @@
     <title>หน้าหลัก</title>
 
     <!-- Custom fonts for this template-->
+    <link rel="icon" type="image/png" href="../../img/home.png">
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Kanit:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
