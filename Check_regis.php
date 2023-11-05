06e39168f4a97ac6c20d9aa03cb954e78682fccb
paradise-j <warastudio61@gmail.com>
2023-11-05 18:27:22 +0700

    Update Check_regis.php

--- Check_regis.php
+++ Check_regis.php
@@ -4,6 +4,22 @@
     session_start();
     require_once "connect.php";
 
+    $prename = $_POST['prename']; echo $prename;
+    $Fname = $_POST['Fname'];
+    $Lname = $_POST['Lname'];
+    $Bdate = $_POST['Bdate']; echo $Bdate;
+    $personid = $_POST['personid'];
+    $email = $_POST['email'];
+    $tel = $_POST['tel'];
+    $Homenumber = $_POST['Homenumber'];
+    $moo = $_POST['moo'];
+    $provinces = $_POST['provinces'];
+    $amphures = $_POST['amphures'];
+    $districts = $_POST['districts'];
+    $zipcode = $_POST['zipcode'];
+    $username = $_POST['personid'];
+    $password = $_POST['tel'];
+
     if (isset($_POST['submit'])) {
         $prename = $_POST['prename'];
         $Fname = $_POST['Fname'];
@@ -25,8 +41,8 @@
                                                      `mem_Bdate`, `mem_perid`, `mem_homenum`, 
                                                      `mem_moo`, `mem_subdis`, `mem_dis`, 
                                                      `mem_province`, `mem_zipcode`, `mem_username`, `mem_password`)
-                                                     VALUES ('$prename','$Fname','$Lname','$Bdate','$personid',
-                                                             '$email','$tel','$Homenumber','$moo','$districts',
+                                                     VALUES ('$prename','$Fname','$Lname','$email','$tel',
+                                                             '$Bdate','$personid','$Homenumber','$moo','$districts',
                                                              '$amphures','$provinces','$zipcode','$username','$password')");
 
         $sql->execute();
