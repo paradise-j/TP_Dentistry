40be19266aa17ab72eeb3b0207bd5ae5208aeb6c
paradise-j <warastudio61@gmail.com>
2023-11-06 16:08:20 +0700

    Update index.php

--- role/admin/index.php
+++ role/admin/index.php
@@ -31,7 +31,7 @@
 
         <div id="content-wrapper" class="d-flex flex-column">
             <div id="content">
-                <div class="container-fluid mt-4">
+                <div class="container-fluid mt-5">
                     <div class="row">
                         <!-- Earnings (Monthly) Card Example -->
                         <div class="col-xl-3 col-md-6 mb-4">
